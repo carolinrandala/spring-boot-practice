f2308a285ba757bcc0cf438343b1175624b6e7df
carolin randala <carolinrandala@gmail.com>
2023-03-06 22:50:47 +0200

    homework started with student controller

--- src/main/java/com/sda/study/springbootpractice/controllers/StudentController.java
+++ src/main/java/com/sda/study/springbootpractice/controllers/StudentController.java
@@ -1,9 +1,16 @@
 package com.sda.study.springbootpractice.controllers;
 
+import com.sda.study.springbootpractice.exceptions.CourseNotFoundException;
+import com.sda.study.springbootpractice.exceptions.StudentNotFoundException;
 import com.sda.study.springbootpractice.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/student")
@@ -11,4 +18,25 @@ public class StudentController {
 
     @Autowired
     private StudentService studentService;
+
+    @GetMapping
+    public String showStudentListPage(Model model, @ModelAttribute("message")String message, @ModelAttribute("messageType") String messageType) { // to send data always need model
+        model.addAttribute("students", studentService.findAllStudents()); // assigns data to attribute name schools
+        return "student/list-student";
+    }
+    @GetMapping("/{id}") // path variable
+    public String showStudentViewPage(@PathVariable Long id, Model model, RedirectAttributes redirectAttributes) {
+        try {
+            model.addAttribute("student", studentService.findStudentById(id));
+            return "student/view-student";
+        } catch (StudentNotFoundException e) {
+            return handleException(redirectAttributes, e);
+        }
+    }
+    // PRIVATE METHODS
+    private String handleException(RedirectAttributes redirectAttributes, Exception e) {
+        redirectAttributes.addFlashAttribute("message", e.getLocalizedMessage());
+        redirectAttributes.addFlashAttribute("messageType", "error");
+        return "redirect:/course";
+    }
 }
