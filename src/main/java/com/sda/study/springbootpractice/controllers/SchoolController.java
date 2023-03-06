a662208ca71b595bba38e9b42ad45def28a8194f
carolin randala <carolinrandala@gmail.com>
2023-03-06 20:33:38 +0200

    Update and create school finished

--- src/main/java/com/sda/study/springbootpractice/controllers/SchoolController.java
+++ src/main/java/com/sda/study/springbootpractice/controllers/SchoolController.java
@@ -66,42 +66,54 @@ public class SchoolController {
 
     }
 
-
-    @GetMapping("/create/{id}")
-    public String createSchoolPage(@PathVariable Long id, RedirectAttributes redirectAttributes) {
-
-        schoolService.createSchool(new School());
-        redirectAttributes.addFlashAttribute("message", String.format("School #name=%s created successfully", id));
-        redirectAttributes.addFlashAttribute("messageType", "success");
-        return "/school/create-school";
-
+    // To show the create school form page
+    @GetMapping("/create")
+    public String showCreateSchoolPage(@ModelAttribute("school") School school, @ModelAttribute("message") String message, @ModelAttribute("messageType") String messageType) {
+        return "school/create-school";
     }
 
-    /*
+    // Called when we press submit button in the create school form
+    @PostMapping
+    public String createSchoolPage(School school, RedirectAttributes redirectAttributes) {
+        try {
+            School searchSchool = schoolService.findSchoolByName(school.getName());
+            redirectAttributes.addFlashAttribute("message", String.format("School (%s) already exists!", school.getName()));
+            redirectAttributes.addFlashAttribute("messageType", "error");
+            return "redirect:/school/create-school";
+        } catch (SchoolNotFoundException e) {
+            schoolService.createSchool(school);
+            redirectAttributes.addFlashAttribute("message", String.format("School (%s) has been created successfully!", school.getName()));
+            redirectAttributes.addFlashAttribute("messageType", "success");
+            return "redirect:/school";
+        }
+    }
+
     @GetMapping("/update/{id}")
-    public String updateSchool(@ModelAttribute School school, @PathVariable Long id,  RedirectAttributes redirectAttributes) {
+    public String showUpdateSchoolPage(@PathVariable Long id, RedirectAttributes redirectAttributes,
+                                       @RequestParam(value = "school", required = false) School school, // only if you update the record
+                                       Model model){
+        if (school == null) {
+            try {
+                model.addAttribute("school", schoolService.findSchoolById(id));
+            } catch (SchoolNotFoundException e) {
+                return handleException(redirectAttributes, e);
+            }
+        }
+        return "school/update-school";
+    }
+
+    @PostMapping("/update")
+    public String updateSchool(School school, RedirectAttributes redirectAttributes) {
         try {
             schoolService.updateSchool(school);
-            redirectAttributes.addFlashAttribute("message", String.format("School #%d is updated successfully!", id));
+            redirectAttributes.addFlashAttribute("message", String.format("School(id=%d) has been updated successfully!", school.getId()));
             redirectAttributes.addFlashAttribute("messageType", "success");
-            return "redirect:/school-update";
+            return "redirect:/school";
         } catch (SchoolNotFoundException e) {
             return handleException(redirectAttributes, e);
         }
     }
 
-     */
-    @GetMapping("/update/{name}")
-    public String updateSchoolPage(@PathVariable String name, RedirectAttributes redirectAttributes) {
-        try {
-            schoolService.updateSchool(schoolService.findSchoolByName(name));
-            redirectAttributes.addFlashAttribute("message", String.format("School #name=%s updated successfully", name));
-            redirectAttributes.addFlashAttribute("messageType", "success");
-            return "/school/update-school";
-        } catch (SchoolNotFoundException e) {
-            return handleException(redirectAttributes, e);
-        }
-    }
 
 
 
