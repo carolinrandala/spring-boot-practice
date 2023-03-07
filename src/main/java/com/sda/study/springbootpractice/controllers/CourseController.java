6051d81e638407af59abd81046c6062c2d5fe668
carolin randala <carolinrandala@gmail.com>
2023-03-07 21:02:43 +0200

    RestController done for Course

--- src/main/java/com/sda/study/springbootpractice/controllers/CourseController.java
+++ src/main/java/com/sda/study/springbootpractice/controllers/CourseController.java
@@ -1,112 +1,76 @@
 package com.sda.study.springbootpractice.controllers;
 
 import com.sda.study.springbootpractice.exceptions.CourseNotFoundException;
+import com.sda.study.springbootpractice.exceptions.SchoolNotFoundException;
 import com.sda.study.springbootpractice.models.Course;
+import com.sda.study.springbootpractice.models.School;
 import com.sda.study.springbootpractice.services.CourseService;
+import com.sda.study.springbootpractice.services.SchoolService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@Controller
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
 @RequestMapping("/course")
 public class CourseController {
     @Autowired
     private CourseService courseService;
 
+    @Autowired
+    private SchoolService schoolService;
+
     @GetMapping
-    public String showCourseListPage(Model model, @ModelAttribute("message")String message, @ModelAttribute("messageType") String messageType) { // to send data always need model
-        model.addAttribute("courses", courseService.findAllCourses()); // assigns data to attribute name schools
-        return "course/list-course";
+    public List<Course> findAllCourses() {
+        return courseService.findAllCourses();
     }
-    @GetMapping("/{id}") // path variable
-    public String showCourseViewPage(@PathVariable Long id, Model model, RedirectAttributes redirectAttributes) {
-        try {
-            model.addAttribute("course", courseService.findCourseById(id));
-            return "course/view-course";
-        } catch (CourseNotFoundException e) {
-            return handleException(redirectAttributes, e);
-        }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findCourseById(@PathVariable Long id) throws CourseNotFoundException {
+        Course course = courseService.findCourseById(id);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        httpHeaders.setDate(new Date().toInstant());
+        return new ResponseEntity<>(course, httpHeaders, HttpStatus.OK);
+
     }
 
     @GetMapping("/delete/{id}")
-    public String deleteCourse(@PathVariable Long id, RedirectAttributes redirectAttributes) {
-
-            try {
-                courseService.deleteCourseById(id);
-            } catch (CourseNotFoundException e) {
-                return handleException(redirectAttributes, e);
-            }
-            redirectAttributes.addFlashAttribute("message", String.format("Course #%d deleted successfully", id));
-            redirectAttributes.addFlashAttribute("messageType", "success");
-            return "redirect:/course"; // if you want to redirect page back
+    public ResponseEntity<?> deleteCourse(@PathVariable Long id) throws CourseNotFoundException {
+        courseService.deleteCourseById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
         }
 
     @GetMapping("/restore/{id}")
-    public String restoreCourse(@PathVariable Long id, RedirectAttributes redirectAttributes) {
-        try {
-            courseService.restoreCourseById(id);
-            redirectAttributes.addFlashAttribute("message", String.format("Course #%d restored successfully", id));
-            redirectAttributes.addFlashAttribute("messageType", "success");
-            return "redirect:/course"; // if you want to redirect page back
-        } catch (CourseNotFoundException e) {
-            return handleException(redirectAttributes, e);
-        }
+    public ResponseEntity<?> restoreCourse(@PathVariable Long id) throws CourseNotFoundException {
+        courseService.restoreCourseById(id);;
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @GetMapping("/create")
-    public String showCreateCoursePage(@ModelAttribute("course")Course course, @ModelAttribute("message") String message, @ModelAttribute("messageType") String messageType) {
-        return "course/create-course";
-    }
-
-    // Called when we press submit button in the create school form
     @PostMapping
-    public String createCoursePage(Course course, RedirectAttributes redirectAttributes) {
+    public ResponseEntity<?> createCourse(@RequestBody Course course) {
         try {
             Course searchCourse = courseService.findCourseByName(course.getName());
-            redirectAttributes.addFlashAttribute("message", String.format("Course (%s) already exists!", course.getName()));
-            redirectAttributes.addFlashAttribute("messageType", "error");
-            return "redirect:/course/create-course";
+            throw new RuntimeException("Course already exists! Cannot create this course");
         } catch (CourseNotFoundException e) {
             courseService.createCourse(course);
-            redirectAttributes.addFlashAttribute("message", String.format("Course (%s) has been created successfully!", course.getName()));
-            redirectAttributes.addFlashAttribute("messageType", "success");
-            return "redirect:/course";
+
+            return new ResponseEntity<>(HttpStatus.CREATED);
         }
     }
 
-    @GetMapping("/update/{id}")
-    public String showUpdateCoursePage(@PathVariable Long id, RedirectAttributes redirectAttributes,
-                                       @RequestParam(value = "course", required = false) Course course,
-                                       Model model){
-        if (course == null) {
-            try {
-                model.addAttribute("course", courseService.findCourseById(id));
-            } catch (CourseNotFoundException e) {
-                return handleException(redirectAttributes, e);
-            }
-        }
-        return "course/update-course";
-    }
 
     @PostMapping("/update")
-    public String updateCourse(Course course, RedirectAttributes redirectAttributes) {
-        try {
-            courseService.updateCourse(course);
-            redirectAttributes.addFlashAttribute("message", String.format("Course(id=%d) has been updated successfully!", course.getId()));
-            redirectAttributes.addFlashAttribute("messageType", "success");
-            return "redirect:/course";
-        } catch (CourseNotFoundException e) {
-            return handleException(redirectAttributes, e);
-        }
+    public ResponseEntity<?> updateCourse(@RequestBody Course course) throws CourseNotFoundException {
+        courseService.updateCourse(course);
+        return new ResponseEntity<>(HttpStatus.OK);
+
     }
 
-
-    // PRIVATE METHODS
-    private String handleException(RedirectAttributes redirectAttributes, Exception e) {
-        redirectAttributes.addFlashAttribute("message", e.getLocalizedMessage());
-        redirectAttributes.addFlashAttribute("messageType", "error");
-        return "redirect:/course";
-    }
 }
--- src/main/java/com/sda/study/springbootpractice/controllers/SchoolController.java
+++ src/main/java/com/sda/study/springbootpractice/controllers/SchoolController.java
@@ -9,10 +9,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 import java.util.Date;
 import java.util.List;
